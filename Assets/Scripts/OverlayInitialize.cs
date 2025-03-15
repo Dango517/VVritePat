d47020849caa355ae233134e0cfdc151e8522dfe
dang0 <dang0vrc@gmail.com>
2025-03-15 18:44:25 +0900

    add: InitializeScript

--- Assets/Scripts/OverlayInitialize.cs
+++ Assets/Scripts/OverlayInitialize.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
-public class OverlayInitialize : MonoBehaviour
-{
+public class OverlayInitialize : MonoBehaviour {
+
+    void Awake() {
+        InitOpenVR();
+    }
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
+    
+    private void OnDestroy() {
+        CleanUp();
+    }
+
+    void InitOpenVR(){
+        if(OpenVR.System != null)
+            return;
+        
+        var error = EVRInitError.None;
+        OpenVR.Init(ref error, EVRApplicationType.VRApplication_Overlay);
+        if(error != EVRInitError.None) {
+            throw new System.Exception("Failed to init OpenVR");
+        }
+    }
+
+    void CleanUp(){
+        if(OpenVR.System == null)
+            return;
+        OpenVR.Shutdown();
+    }
+
 }
